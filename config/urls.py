92cf97725f1872140ec9f9281fc60b7444be65f3
benjamin33 <bar3243@gmail.com>
2023-06-14 21:41:48 -0400

    'inicio' added to main urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from vehiculo.views import index
 
 urlpatterns = [
+    path('inicio/', index, name='inicio'),
     path('admin/', admin.site.urls),
     path('', include('vehiculo.urls')),
     path('', include('login.urls'))
